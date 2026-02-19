7948e15ffd8d3bf8e9cda852b9430bfff2b27c12
Nikyn Suresh <s.nikyn005@gmail.com>
2026-02-19 12:05:22 +0530

    Add condition to check solution path in backtrack

--- rat_escape_problem.py
+++ rat_escape_problem.py
@@ -4,7 +4,7 @@ grid=[[0,1,1,0],
 r,c=len(grid),len(grid[0])
 sol=[[0]*c for i in range(r)]
 def backtrack(i,j,sol):
-    if i==r-1 and j==c-1 and grid[i][j]==0:
+    if i==r-1 and j==c-1 and grid[i][j]==0 and sol[i][j]==0 :
         sol[i][j]=1
         return True
     elif i<0 or j<0 or i>=r or j>=c or grid[i][j]==1:
