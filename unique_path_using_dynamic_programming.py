0c884844a1d8f7348daed826a5a722a3bb4370ce
Nikyn Suresh <s.nikyn005@gmail.com>
2026-02-23 09:43:25 +0530

    Implement uniquePathsWithObstacles method
    Added a method to calculate unique paths in a grid with obstacles using dynamic programming.

--- unique_path_using_dynamic_programming.py
+++ unique_path_using_dynamic_programming.py
@@ -6,3 +6,20 @@ class Solution:
                 dp[i][j]=dp[i-1][j]+dp[i][j-1]
         return dp[m-1][n-1]
 #uses the technique that ways to reach current cell=ways to reach left cell+ways to reach right cell
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        r=len(obstacleGrid)
+        c=len(obstacleGrid[0])
+        dp=[[0]*c for i in range(r)]
+        dp[0][0]=1 if obstacleGrid[0][0]==0 else 0
+        for i in range(r):
+            for j in range(c):
+                if obstacleGrid[i][j]==1:
+                    dp[i][j]=0
+                else:
+                    if i>0:
+                        dp[i][j]+=dp[i-1][j]
+                    if j>0:
+                        dp[i][j]+=dp[i][j-1]
+        return dp[r-1][c-1]
+#same unique path with obsctacle 1 represents as obstacles
